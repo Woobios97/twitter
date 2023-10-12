a37bff3c7e9810d0469c5e2c572a3b9f76e9b3b3
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2023-10-12 11:41:52 +0900

    Feature/logo추가

---
+++ Twitter/Twitter/Controllers/General/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  Twitter
+//
+//  Created by 김우섭 on 10/12/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Profile"
+    }
+    
+
+
+
+}
